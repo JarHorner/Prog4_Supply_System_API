4f99fa67027f2780a3ed4a2d2fba0b3d24387b10
Jarret Horner <jhorn384@mtroyal.ca>
2022-11-03 10:56:38 -0600

    added try-catch statements to calls

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,40 @@
 module.exports.register = (app, database) => {
-
   //base call, shows the api works
   app.get("/", async (req, res) => {
-    res.status(200).send("The API is connected properly! ").end();
+    try {
+      res.status(200).send("The API is connected properly! ").end();
+    } catch (error) {
+      res
+        .status(error?.status || 500)
+        .send({ status: "FAILED", data: { error: error?.message || error } });
+    }
   });
 
   //retrieves all the items in the database.
   app.get("/api/items", async (req, res) => {
     let query;
-    query = database.query("SELECT * FROM Items");
+    try {
+      query = database.query("SELECT * FROM Items");
 
-    console.log(query);
-    const items = await query;
+      console.log(query);
+      const items = await query;
 
-    res.status(200).send(JSON.stringify(items)).end();
+      res.status(200).send(JSON.stringify(items)).end();
+    } catch (error) {
+      res
+        .status(error?.status || 500)
+        .send({ status: "FAILED", data: { error: error?.message || error } });
+    }
   });
 
   //retrieves a specific item with an id
   app.get("/api/items/:id", async (req, res) => {
-    res.status(200).send("The API will retrieve an item from the database by either name or id!").end();
+    res
+      .status(200)
+      .send(
+        "The API will retrieve an item from the database by either name or id!"
+      )
+      .end();
   });
 
   //Adds a new item to the Item table in the database
@@ -37,15 +53,19 @@ module.exports.register = (app, database) => {
     ) {
       _status = "Unsuccess";
     } else {
-      const query = database.query(
-        "insert into Items(name, stockQuantity, price, supplierId) values (?, ?, ?, ?)",
-        [_name, _stockQuantity, _price, _supplierId]
-      );
-      const items = await query;
-      _status = "Success";
-    }
+      try {
+        database.query(
+          "insert into Items(name, stockQuantity, price, supplierId) values (?, ?, ?, ?)",
+          [_name, _stockQuantity, _price, _supplierId]
+        );
+        _status = "Success";
+      } catch (error) {
+        res
+          .status(error?.status || 500)
+          .send({ status: "FAILED", data: { error: error?.message || error } });
+      }
 
-    let messsage =
+      let messsage =
       '{"status":"' +
       _status +
       '", "data":{"_name":"' +
@@ -59,15 +79,24 @@ module.exports.register = (app, database) => {
       '"}}';
     const obj_messsage = JSON.parse(messsage);
     res.status(200).send(obj_messsage).end();
+    }
   });
 
   // will be able to change the quantity in the database
   app.patch("/api/items/:itemId", async (req, res) => {
-    res.status(200).send("The API will change the quantity of an item in the database by the id!").end();
+    res
+      .status(200)
+      .send(
+        "The API will change the quantity of an item in the database by the id!"
+      )
+      .end();
   });
 
   // will be able to delete an item in the database
   app.delete("/api/items/:itemId", async (req, res) => {
-    res.status(200).send("The API will delete an item in the database by the id!").end();
+    res
+      .status(200)
+      .send("The API will delete an item in the database by the id!")
+      .end();
   });
 };
