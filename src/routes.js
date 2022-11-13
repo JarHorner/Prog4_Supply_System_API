8fa055bd8c9fe054d7097e70fa9e0ae6a0a8c137
Nate P <npena535@mtroyal.ca>
2022-11-12 23:18:15 -0700

    add test delete

--- src/routes.js
+++ src/routes.js
@@ -25,8 +25,101 @@ module.exports.register = (app, database) => {
     res.status(200).send("The API will change the quantity of an item in the database by the id!").end();
   });
 
-  // will be able to delete an item in the database
+  // will be able to delete an item by id in the database
   app.delete("/api/items/:itemId", async (req, res) => {
+    
+    let success = false;
+    let itemID = req.body.id;
+
+    if(NaN(itemID)){
+
+      res.status(400)
+         .send({
+          tried: "Deleting Selected Item",
+          success: success,
+          error: error,
+          message: "Could not process request. Please check if the information provided is correct."
+         })
+
+    } else {
+      try {
+
+        database.query('DELETE FROM items WHERE item_id = ?')[itemID];
+
+      } catch (error){
+
+        switch(error.status){
+          case 404:
+            res.status(404)
+               .send({
+                tried: "Deleting Selected Item",
+                success: success,
+                error: error,
+                message: "Item not found. Please select a valid and existing item."
+               })
+
+          case 500:
+            res.status(500)
+               .send({
+                tried: "Deleting Selected Item",
+                success: success,
+                error: error,
+                message: "Unable to connect to API. Please make sure that the server is up and running."
+               })
+        }
+      }
+    }
+
     res.status(200).send("The API will delete an item in the database by the id!").end();
+
+  });
+
+  // will be able to delete an item by name in the database
+  app.delete("/api/items/:itemName", async (req, res) => {
+    
+    let success = false;
+    let itemName = req.body.id;
+
+    if(NaN(itemName)){
+
+      res.status(400)
+         .send({
+          tried: "Deleting Selected Item",
+          success: success,
+          error: error,
+          message: "Could not process request. Please check if the information provided is correct."
+         })
+
+    } else {
+      try {
+
+        database.query('DELETE FROM items WHERE item_name = ?')[itemName];
+
+      } catch (error){
+
+        switch(error.status){
+          case 404:
+            res.status(404)
+               .send({
+                tried: "Deleting Selected Item",
+                success: success,
+                error: error,
+                message: "Item not found. Please select a valid and existing item."
+               })
+
+          case 500:
+            res.status(500)
+               .send({
+                tried: "Deleting Selected Item",
+                success: success,
+                error: error,
+                message: "Unable to connect to API. Please make sure that the server is up and running."
+               })
+        }
+      }
+    }
+
+    res.status(200).send("The API will delete an item in the database by the id!").end();
+    
   });
 };
