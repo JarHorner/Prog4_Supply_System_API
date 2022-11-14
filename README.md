c1a73056e88560bdbeaf89262a9329c670511af0
Shreshthk1 <54998233+Shreshthk1@users.noreply.github.com>
2022-11-13 23:08:51 -0700

    Dev (#27)
    * Delete .env
    
    * Create .env
    
    * Dev jarret create/retrieve (#9)
    
    * retrieve all items & adding a new item functionallity added
    
    * Update README.md
    
    * added try-catch statements to calls
    
    * Changing back to original format from testing
    
    * Added better error handling
    
    * pushing final changes
    
    * Delete .env
    
    * Dev 7/enviorment (#13)
    
    * Update README.md
    
    * Update README.md
    
    * Added enviorment file
    
    * added database connection
    
    * Added .env to git ignore
    
    * cleared env
    
    Co-authored-by: Jarret Horner <55000163+JarHorner@users.noreply.github.com>
    
    * Delete .env
    
    * Added API DOC
    
    * Changing format of docs
    
    * Formating REV 2
    
    * Formatting test
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * POST DOCS
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Added docs for adding items
    
    * Update README.md
    
    * Patch Docs
    
    * Update README.md
    
    * Finished DOCs
    
    * add test delete
    
    * spelling mistake fix
    
    * API Documentation (#16)
    
    * Added API DOC
    
    * Changing format of docs
    
    * Formating REV 2
    
    * Formatting test
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * POST DOCS
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Added docs for adding items
    
    * Update README.md
    
    * Patch Docs
    
    * Update README.md
    
    * Finished DOCs
    
    * spelling mistake fix
    
    Co-authored-by: Jarret Horner <55000163+JarHorner@users.noreply.github.com>
    
    * Finished get specific item with an id,endpoint
    
    * Dev 4/update item (#17)
    
    * Update README.md
    
    * Update README.md
    
    * Added Error trapping and Prepared Statement
    
    * Added error trapping and implemented responses
    
    * Refactored and tested. Working ok
    
    * updated readme
    
    Co-authored-by: Jarret Horner <55000163+JarHorner@users.noreply.github.com>
    
    * Updated the ReadMe file with Get Items by Id example
    
    * Updated the ReadMe file with Get Items by Id example
    
    * testing delete
    
    * finished delete
    
    * final
    
    * delete part
    
    * removed .env contents added it to .gitignore
    
    * Fixed and added the condition for searching item by name
    
    * final file
    
    * Delete .env
    
    * finished delete
    
    * Updated the read.me
    
    * Updated get all items and default API call
    
    * Update README.md
    
    * Update README.md
    
    * Spelling errors
    
    * Update README.md
    
    Co-authored-by: Jarret Horner <55000163+JarHorner@users.noreply.github.com>
    Co-authored-by: Jasmail Singh Duck <jasmailduck@icloud.com>
    Co-authored-by: Nate P <npena535@mtroyal.ca>
    Co-authored-by: Afilv222 <55000122+Afilv222@users.noreply.github.com>

--- README.md
+++ README.md
@@ -4,45 +4,231 @@ COMP3504_Assign2 is nodejs API for basic functionallity recieving, editing and d
 
 ## Installation
 
+Before everything, make sure the database you are using is up and running. The API will not function otherwise.
+
 - Open terminal in vscode and make sure you are in the react file path
-> .........../ ...../COMP3504_Assign2
+
+  > .........../ ...../COMP3504_Assign2
 
 - Install the dependancies
-`npm install`
+  `npm install`
 
-- ensure the .env file has the correct properties to be able to connect to the proper database. 
-> Open .env in vscode to change
+- ensure the .env file has the correct properties to be able to connect to the proper database.
+
+  > Open .env in vscode to change
 
 - Once all dependancies are installed and .env file is proper, run the API to test
-`npm start`
+  `npm start`
 
--if running on your own machine, go to `http://localhost:8080/` and you should see it working. 
+-if running on your own machine, go to `http://localhost:8080/` and you should see it working.
 if not, make sure to check if the database is running or the .env file has the proper information.
 
 ## Documentation
 
 Here are all the calls you can make using this API:
 
-<br> `GET     /` <br><br>
+### GET
 
-Confirms a connection is made with the API.
 
-<br><br> `GET      /api/items` <br><br>
+- `/`\
+   Gets default API call: Displays status of API
+   
+      Example of a sample URl request:
+      "http://35.209.74.28:8080"
+      
+      
+  > **RESPONSE:** API status response
+    
+    Example of sucess response:
+    `````````
+    The API is connected properly!
 
-Retrieves all the items in the database.
+<br />
 
-<br><br> `GET      /api/items/:id` <br><br>
+- ` /api/items`
 
-Retrieves a certain item from the database by its id.
+   Gets items currently present in database
 
-<br><br> `POST      /api/items` <br><br>
+  >**REQUEST URL:**
 
-Creates an item from the database by posting a JSON body containing the name, stockQuantity, price and supplierId.
+      Example of a sample URl request:
+      "http://35.209.74.28:8080/api/items"
+      
+      
+  > **RESPONSE:** JSON Object
+    
+    Example of sucess response:
+    `````````
+    {
+        "status": "Success",
+        "data": {
+            "item_id": 3001,
+            "item_name": "Widgets",
+            "item_quantity": 10,
+            "item_price": 35.5,
+            "item_supplier_id": "50004"
+        },
+        {
+            "item_id": 3002,
+            "item_name": "Grommets",
+            "item_quantity": 20,
+            "item_price": 23.45,
+            "item_supplier_id": "50001"
+        },
+        {
+            "item_id": 3003,
+            "item_name": "Wedges",
+            "item_quantity": 15,
+            "item_price": 10.15,
+            "item_supplier_id": "50020"
+        }........
+    }
+    `````````
 
-<br><br> `PATCH      /api/items/:id` <br><br>
+<br />
 
-Retrieves a certain item from the database by its id, and changes the stockQuantity by the amount in the given JSON body.
+- ` /api/items/:identifier` \
+    Gets items currently present in database by item ID or Name
 
-<br><br> `DELETE      /api/items/:id` <br><br>
+    ATTRIBUTE | DESCRIPTION
+    --------- | -----------
+    id | Int: a specific number for item id will be returned    
+    name| String: a specific name for item will be returned 
 
-Removes an item from the database by its id.
+    >**REQUEST URL:**
+
+      Example of a sample URl request:
+      "http://35.209.74.28:8080/api/items/3001"
+      "http://35.209.74.28:8080/api/items/Bing Bobs"
+
+
+    > **RESPONSE:**  JSON Object
+    
+    Example of sucess response:
+    `````````
+    {
+        "status": "Success",
+        "data": {
+            "item_id": 3001,
+            "item_name": "Widgets",
+            "item_quantity": 10,
+            "item_price": 35.5,
+            "item_supplier_id": "50004"
+        }
+    }
+    `````````
+     `````````
+    {
+        "status": "Success",
+        "data": {
+           "item_id": 3007,
+          "item_name": "Bing Bobs",
+          "item_quantity": 25,
+          "item_price": 2.39,
+          "item_supplier_id": "50005"
+        }
+    }
+    `````````
+
+
+### POST
+
+- ` /api/items`\
+    Add a new item to the database
+    
+
+    ATTRIBUTE | DESCRIPTION
+    --------- | -----------
+    name | String: a line of text for the item name 
+    stockQuantity | INT: a number describing the stock count of the item 
+    price | DOUBLE: a number describing the price of the item 
+    supplierId | INT: a number for the Id of the supplier
+
+    > **REQUEST BODY:** JSON Object with required attributes:
+
+    Example of a sample JSON body request:
+    ``````
+    {
+        "name" : "test",
+        "stockQuantity" : 8,
+        "price" : 9.80,
+        "supplierId" : 5010
+    }
+    ``````
+    > **RESPONSE:**  JSON Object
+
+    Example of sucess response:
+    `````````
+    {
+        "status": "Success",
+        "data": {
+            "_name": "test",
+            "_stockQuantity": "8",
+            "_price": "9.8",
+            "_supplierId": "5010"
+        }
+    }
+    `````````
+
+<br />
+
+    
+<br />
+
+### DELETE
+
+- ` /api/items/delete/:id` \
+    Deletes the item from the database using the item id
+    ATTRIBUTE | DESCRIPTION
+    --------- | -----------
+    item_id   | Item ID of an existing item
+    
+     Example of sucess response:
+    > **REQUEST BODY:** JSON Object with new attributes:
+     
+    Example of a sample JSON body request:
+    ``````
+    {
+        "item_id" : 3040
+    }
+    ``````
+    > **RESPONSE:**  JSON Object
+    
+
+    Example of success response:
+    `````````
+    {
+        "success": True
+    }
+    `````````
+
+### PATCH
+
+- ` /api/items/:id`\
+   Change the quantity of an item using it's Id in the database, like quantity for example
+
+  | ATTRIBUTE         | DESCRIPTION                  |
+  | ----------------- | ---------------------------- |
+  | quantity          | New value                    |
+
+  > **REQUEST BODY:** JSON Object with new attributes:
+
+  Example of a sample JSON body request:
+
+  ```
+  {
+      "quantity" : 10
+  }
+  ```
+
+  > **RESPONSE:** JSON Object
+
+  Example of sucess response:
+
+  ```
+  {
+      "success": "True",
+  }
+  ```
+
+<br />
--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,244 @@
-module.exports.register = (app, database) => {
 
+const { query } = require("express");
+
+module.exports.register = (app, database) => {
   //base call, shows the api works
   app.get("/", async (req, res) => {
-    res.status(200).send("The API is connected properly! ").end();
+    try {
+      res.status(200).send("The API is connected properly! ").end();
+    } catch (error) {
+      res.status(error?.status).send({
+        tried: "Base call",
+        status: `FAILED`,
+        error: error?.message || error,
+        message: "Not properly connected to the API",
+        detail: "Ensure you are correctly connected to the API",
+      });
+    }
   });
+
+
 
   //retrieves all the items in the database.
   app.get("/api/items", async (req, res) => {
-    res.status(200).send("The API will retrieve all the items from the database!").end();
-  });
+    let query;
+    try {
+      query = database.query("SELECT * FROM items");
 
-  //retrieves a specific item with an id
-  app.get("/api/items/:id", async (req, res) => {
-    res.status(200).send("The API will retrieve an item from the database by either name or id!").end();
+      console.log(query);
+      const items = await query;
+
+      res.status(200).send(JSON.stringify(items)).end();
+    } catch (error) {
+      res
+        .status(error?.status)
+        .send({ 
+          tried: 'Retrieving all items', 
+          status: `FAILED`, 
+          error: error?.message || error,
+          message: 'Not properly connected to the API' ,
+          detail: 'Ensure you are correctly connected to the API'
+        });
+    }
   });
+  
+  // will be able to delete an item in the database
+  app.delete("/api/items/delete/:id", async (req, res) => {
+    let itemID = req.params.id
+    let success = false;
+
+    if(!isNaN(itemID)) {
+
+      database.query(`DELETE FROM items WHERE item_id = ?`, [itemID], (errors, results, fields) => {
+
+        if(errors != null){
+          if(errors.code == 'ETIMEDOUT') {
+            res.status(500).send({
+              tried: "Deleting Item",
+              success: success,
+              message: "Unable to connect to API. Please make sure server is running."
+            }).end();
+            return;
+          }
+        }
+
+        if(results.affectedRows == 0) {
+          res.status(404).send({
+            tried: "Deleting Item",
+            success: success,
+            message: "Item does not exist. Please delete an existing item."
+          }).end();
+          return;
+        }
+        
+        res.status(200).send({ success: !success }).end();
+        
+      });
+    } else {
+      res.status(400).send({
+        tried: "Deleting Item",
+        success: success,
+        messsage:
+          "Could not process request. Please check if the information provided is correct",
+      });
+    }
+
+  //retrieves a specific item with an id or add
+  app.get("/api/items/:identifier" , async (req, res) => {
+    
+    let query = ""; 
+    let _identifier = req.params.identifier;
+    let items; 
+    let _status = 500;
+
+    if(isNaN(_identifier)){
+      query = database.query("SELECT * FROM items WHERE item_name =?", [_identifier]);
+    }else{
+      query = database.query("SELECT * FROM items WHERE item_id =?", [_identifier]);
+    }
+
+    try {
+         items = await query;
+        
+        if(items.length != 0) {
+          res.status(200).send(JSON.stringify(items)).end()
+        }else{
+           _status = 400; 
+           throw error  
+        }
+      
+        
+    } catch (error) {
+      if(_status === 400){
+        res
+        .status(_status)
+        .send({
+            tried: 'The API will retrieve an item from the database by either name or id!', 
+            status: "FAILED", 
+            error: error?.message || error,
+            message: 'The id or name entered in is either mis-formatted or incorrect' ,
+            detail: 'Ensure you are including a correct id or name that exists'
+          });
+      }else if(_status === 500){
+        res
+      .status(_status)
+      .send({
+          tried: 'The API will retrieve an item from the database by either name or id!', 
+          status: `FAILED`, 
+          error: error?.message || error,
+          message: 'Not properly connected to the API' ,
+          detail: 'Ensure you are correctly connected to the API'
+        });
+      }
+    }
+  
+  }
+      
+      
+  );
 
   //Adds a new item to the Item table in the database
   app.post("/api/items", async (req, res) => {
-    res.status(200).send("The API will add a new item to the database!").end();
+    let _name = req.body.name;
+    let _stockQuantity = req.body.stockQuantity;
+    let _price = req.body.price;
+    let _supplierId = req.body.supplierId;
+    let _status = "";
+
+    if (
+      typeof _name === "undefined" ||
+      typeof _stockQuantity === "undefined" ||
+      typeof _price === "undefined" ||
+      typeof _supplierId === "undefined"
+    ) {
+      _status = "Unsuccess";
+    } else {
+      try {
+        database.query(
+          "insert into Items(name, stockQuantity, price, supplierId) values (?, ?, ?, ?)",
+          [_name, _stockQuantity, _price, _supplierId]
+        );
+        _status = "Success";
+      } catch (error) {
+        if (error?.status === 400) {
+          res.status(error?.status).send({
+            tried: "Adding a new item",
+            status: `FAILED`,
+            error: error?.message || error,
+            message:
+              "Information in the body is either mis-formatted or incorrect",
+            detail:
+              "Ensure you are including the correct information in the body, and in the right order",
+          });
+        } else if (error?.status === 500) {
+          res.status(error?.status).send({
+            tried: "Adding a new item",
+            status: `FAILED`,
+            error: error?.message || error,
+            message: "Not properly connected to the API",
+            detail: "Ensure you are correctly connected to the API",
+          });
+        }
+      }
+
+      let messsage =
+        '{"status":"' +
+        _status +
+        '", "data":{"_name":"' +
+        _name +
+        '","_stockQuantity":"' +
+        _stockQuantity +
+        '","_price":"' +
+        _price +
+        '", "_supplierId":"' +
+        _supplierId +
+        '"}}';
+      const obj_messsage = JSON.parse(messsage);
+      res.status(200).send(obj_messsage).end();
+    }
   });
 
   // will be able to change the quantity in the database
-  app.patch("/api/items/:itemId", async (req, res) => {
-    res.status(200).send("The API will change the quantity of an item in the database by the id!").end();
+  app.patch("/api/items/:id", async (req, res) => {
+    let success = false;
+    let itemID = req.params.id;
+    let newQuantity = req.body.quantity;
+    if (isNaN(itemID) || isNaN(newQuantity)) {
+      res.status(400).send({
+        tried: "Updating Item Quantity",
+        success: success,
+        messsage:
+          "Could not process request. Please check if the information provided is correct",
+      });
+    } else {
+        database.query(
+          "UPDATE items SET item_quantity = ? WHERE item_id = ?",
+          [newQuantity, itemID],
+          (errors, results, fields) => {
+            if (errors != undefined) {
+              res.status(500).send({
+                tried: "Updating Item Quantity",
+                success: success,
+                error: errors.message,
+                messsage:
+                  "Unable to connect to API. Please ensure the server is running",
+              }).end();
+            }
+            console.log(results);
+            if (results.affectedRows == 0) {
+              res.status(404).send({
+                tried: "Updating Item Quantity",
+                success: success,
+                error: errors,
+                messsage: "Item not found. Please add the Item first",
+              }).end();
+            }
+            else {
+              res.status(200).send({ success: !success }).end();
+            }
+          }
+        );
+      }
   });
-
-  // will be able to delete an item in the database
-  app.delete("/api/items/:itemId", async (req, res) => {
-    res.status(200).send("The API will delete an item in the database by the id!").end();
   });
 };
